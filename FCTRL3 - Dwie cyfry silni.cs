53645391c74691c870dd8975ed0fa6c8e44e2e79
tyburski <129940051+tyburski@users.noreply.github.com>
2024-10-12 13:57:22 +0200

    Add files via upload

---
+++ FCTRL3 - Dwie cyfry silni.cs
@@ -0,0 +1,24 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    int input = int.Parse(Console.ReadLine());
+    if(input < 10)
+    {
+        int c = 1;
+        for (int j = 1; j <= input; j++)
+        {
+            c *= j;
+        }
+        int dziesiatki = (c / 10) % 10;
+        int jednosci = c % 10;
+        Console.WriteLine($"{dziesiatki} {jednosci}");
+    }
+    else
+    {
+        Console.WriteLine("0 0");
+    }
+
+
+}
+
